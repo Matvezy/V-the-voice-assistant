0f4a8f616ff8912ec7aec3c579b97156305b180c
Matvezy <popovm652@gmail.com>
2019-06-17 03:10:09 -0700

    Update vCode.py
    Changed bugs, still not there

--- vCode.py
+++ vCode.py
@@ -1,7 +1,7 @@
 import os
 import time
 import speech_recognition as sr
-from fuzzywuzzy import fuzz
+import fuzzywuzzy
 import pyttsx3
 import datetime
 def speak(what):
@@ -20,30 +20,30 @@ def callback(recognizer,audio):
                 cmd=cmd.replace(x,'').strip()
             for x in opts['tbs']:
                 cmd=cmd.replace(x,'').strip()
-            cmd=recognize_cmd(cmd)
-            execute_cmd(cmd['cmd'])
+        cmd=recognize_cmd(cmd)
+        execute_cmd(cmd['cmd'])
     except sr.UnknownValueError:
-        print('[log] Can't understand the voice!)')
+        print('[log] Cant understand the voice!)')
     except sr.RequestError as e:
         print('[log] Unknown Error')
 def recognize_cmd(cmd):
     RC={cmd:'','percent':0}
-    for c,v in opts['cmds'].items():
+    for c,v in opts['cmd'].items():
         for x in v:
             vert=fuzz.ratio(cmd,x)
             if vrt>RC['percent']:
-                RC['cmd']=c
+                RC[cmd]=c
                 RC['percent']=vrt
     return RC
 def execute_cmd(cmd):
     if cmd=='ctime':
         now=datetime.datetime.now()
-        speak('It is'+str(now.hour)+':'+str(now.minute))
+        speak('It is:'+str(now.hour)+':'+str(now.minute))
 opts={'alias':('v','vi','ve','ve','vi'),'tbs':('show','open','what'),
-      'cmds':{'ctime':('time','what time is it','tell me the time'),
+      'cmd':{'ctime':('time','what time is it','tell me the time'),
             'radio':('turn on the music','music')} }
 r=sr.Recognizer()
-m=sr.Microphone(device_index=0)
+m=sr.Microphone(device_index=1)
 with m as source:
     r.adjust_for_ambient_noise(source)
 speak_engine=pyttsx3.init()
